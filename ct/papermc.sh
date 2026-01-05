e09cac71436857ce51bf494cce2988bf65db29b5
Dmytro Vorotyntsev <2937451+vorotech@users.noreply.github.com>
2026-01-05 09:27:25 +0200

    minor updates

--- ct/papermc.sh
+++ ct/papermc.sh
@@ -73,5 +73,5 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Conect it with Minecraft Client using the following address:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}${IP}:19132${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:7867${CL}"
